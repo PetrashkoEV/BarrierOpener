25c5dcf7e46d6fd414af6f325a6f1b58ca7bda55
Eugen Petrashko <PetrashkoEV@gmail.com>
2024-04-09 15:43:15 +0300

    Delete secrets

--- BarrierOpener.Client/MainPage.xaml.cs
+++ BarrierOpener.Client/MainPage.xaml.cs
@@ -5,14 +5,13 @@ namespace BarrierOpener.Client;
 
 public partial class MainPage : ContentPage
 {
-    private readonly Guid _secret = new("1e7e5e8c-166b-411c-a16f-6bca38cf8dd6");
-    private readonly string _resourceName = "action";
-
     private readonly IFirebaseRepository _firebaseRepository;
+    private readonly IFirebaseConfiguration _configuration;
 
-    public MainPage(IFirebaseRepository firebaseRepository)
+    public MainPage(IFirebaseRepository firebaseRepository, IFirebaseConfiguration configuration)
     {
         _firebaseRepository = firebaseRepository;
+        _configuration = configuration;
         InitializeComponent();
     }
 
@@ -22,11 +21,11 @@ public partial class MainPage : ContentPage
         var message = new BarrierOpenMessage
         {
             RequestDateTime = DateTime.UtcNow,
-            SecretKey = _secret,
+            SecretKey = _configuration.SecretKey,
             DeviceName =
                 $"{DeviceInfo.Current.Model};{DeviceInfo.Current.Name};{DeviceInfo.Current.Idiom}{DeviceInfo.Current.VersionString}"
         };
-        _firebaseRepository.SendMessage(_resourceName, message);
+        _firebaseRepository.SendMessage(_configuration.ResourceName, message);
         OpenerBtn.DisableLayout = false;
     }
 }
--- BarrierOpener.Client/MauiProgram.cs
+++ BarrierOpener.Client/MauiProgram.cs
@@ -1,6 +1,9 @@
 ﻿using BarrierOpener.Domain.Core;
 using BarrierOpener.Domain.Repository;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
+using BarrierOpener.Domain.Configuration;
 
 namespace BarrierOpener.Client;
 
@@ -15,9 +18,11 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+            })
+            .BuildConfiguration();
 
         builder.Services.AddSingleton<IFirebaseRepository, FirebaseRepository>();
+        builder.Services.AddSingleton<IFirebaseConfiguration, FirebaseConfiguration>();
         builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
@@ -25,5 +30,20 @@ public static class MauiProgram
 #endif
 
         return builder.Build();
+    }
+
+    private static MauiAppBuilder BuildConfiguration(this MauiAppBuilder builder)
+    {
+        var a = Assembly.GetExecutingAssembly();
+        using var stream = a.GetManifestResourceStream("BarrierOpener.Client.appsettings.json");
+
+        var config = new ConfigurationBuilder()
+            .AddJsonStream(stream)
+            .Build();
+
+
+        builder.Configuration.AddConfiguration(config);
+
+        return builder;
     }
 }
